0b05578e24c80460471905af6c5360eed6e4313f
SzymonCogiel <cogiel@student.agh.edu.pl>
2023-03-02 19:36:13 +0100

    04_login_form

--- 01_platform/04_login_form/project/index.php
+++ 01_platform/04_login_form/project/index.php
@@ -1,4 +1,18 @@
 <?php
+$user = $_GET["user"];
+$password = $_GET["password"];
 
-echo "Hello!";
-// TODO: ...
+if ($user=="" or $password=="")
+{
+    echo "<p>EMPTY</p>";
+    return "EMPTY";
+}
+if ($user=="foo" and $password=="foo123")
+{
+    echo "<p>OK</p>";
+    return "OK";
+}
+{
+    echo "<p>ERROR</p>";
+    return "ERROR";
+}
