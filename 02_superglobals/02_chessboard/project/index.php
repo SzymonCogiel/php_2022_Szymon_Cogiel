2acc49435b631fad77ec5bd6f4cdbbbfc341a2cc
SzymonCogiel <cogiel@student.agh.edu.pl>
2023-03-12 12:58:05 +0100

    sec

--- 02_superglobals/02_chessboard/project/index.php
+++ 02_superglobals/02_chessboard/project/index.php
@@ -1,89 +1,230 @@
 <?php
 error_reporting(-1);
 ini_set("display_errors", "On");
+
+session_start();
+
+$sx = $_SESSION["sx"] ?? 10;
+$sz = $_SESSION["sz"] ?? 10;
+$col = $_SESSION["col"] ?? array();
+$counter = $_SESSION["count"] ?? 0;
+$x = $_SESSION["x"] ?? 0;
+$y = $_SESSION["y"] ?? 0;
+$color = $_COOKIE["color"] ?? "gray";
+
+if(isset($_POST["color"]))
+{
+    //$color =  $_POST['color'] == "" ? $_COOKIE["color"] : $_POST["color"];
+    $color = $_POST["color"];
+    setcookie("color", $color);
+}
+
 ?>
-<html lang="en">
-<head>
-    <title>Superglobals</title>
 
-    <style type="text/css">
-        .block {
-            display: inline-block;
-            width: 30px;
-            height: 30px;
-            padding: 0;
-            margin: 0;
+    <html lang="en">
+    <head>
+        <title>Superglobals</title>
+
+        <style type="text/css">
+            .block {
+                display: inline-block;
+                width: 30px;
+                height: 30px;
+                padding: 0;
+                margin: 0;
+            }
+
+            .block:hover {
+                background-color: lightgray;
+            }
+
+            .red {
+                background-color: red;
+            }
+
+            .blue {
+                background-color: blue;
+            }
+
+            .green {
+                background-color: green;
+            }
+
+            .gray {
+                background-color: gray;
+            }
+
+            .white {
+                background-color: white;
+            }
+        </style>
+    </head>
+    <body>
+
+
+    <?php
+
+    if(isset($_GET["x"]))
+    {
+        Count_Click();
+        if($counter==2)
+        {
+            $counter = 0;
+            Bresenham($x??0,$y??0,$_GET["x"],$_GET["z"]);
         }
-
-        .block:hover {
-            background-color: lightgray;
+        else
+        {
+            $x = $_GET["x"];
+            $y = $_GET["z"];
         }
+    }
 
-        .red {
-            background-color: red;
+    if(isset($_POST["sz"]))
+    {
+        $sz = $_POST["sz"] == "" ? $_SESSION["sz"] : $_POST["sz"];
+    }
+
+    if(isset($_POST["sx"]))
+    {
+        $sx = $_POST["sx"] == "" ? $_SESSION["sx"] : $_POST["sx"];
+    }
+
+    for($i = 0; $i < $sz; $i++)
+    {
+        echo "<div>";
+        for ($ii = 0; $ii<$sx; $ii++)
+        {
+            if(!isset($col["$ii"]["$i"]))
+            {
+                $col["$ii"]["$i"] = $color;
+            }
+
+            if($col["$ii"]["$i"] != "white")
+            {
+                $col["$ii"]["$i"] = $color;
+            }
+
+            $temp = $col["$ii"]["$i"];
+
+            echo "<a class=\"block $temp\" href=\"?x=$ii&z=$i\"></a>";
         }
+        echo "</div>";
+    }
 
-        .blue {
-            background-color: blue;
+    ?>
+
+    <!--
+    <div>
+        <a class="block gray" href="?x=0&z=1"></a>
+        <a class="block gray" href="?x=1&z=1"></a>
+        <a class="block gray" href="?x=2&z=1"></a>
+    </div>
+    <div>
+        <a class="block gray" href="?x=0&z=2"></a>
+        <a class="block gray" href="?x=1&z=2"></a>
+        <a class="block gray" href="?x=2&z=2"></a>
+    </div>
+    -->
+
+    <br/>
+
+    <form method="post">
+        <label>
+            Columns:
+            <input type="text" name="sx">
+        </label>
+        <label>
+            Rows:
+            <input type="text" name="sz">
+        </label>
+        <input type="submit" value="Change">
+    </form>
+
+    <form method="post">
+        <label>
+            Color:
+            <select name="color">
+                <option value="gray">Gray</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+            </select>
+        </label>
+        <input type="submit" value="Change">
+    </form>
+
+    <?php
+    $_SESSION["sx"] = $sx;
+    $_SESSION["sz"] = $sz;
+    $_SESSION["col"] = $col;
+    $_SESSION["count"] = $counter;
+    $_SESSION["x"] = $x;
+    $_SESSION["y"] = $y;
+    ?>
+    </body>
+    </html>
+
+<?php
+function Count_Click()
+{
+    global $counter;
+    $counter +=1;
+}
+
+function Bresenham($x1, $y1, $x2, $y2)
+{
+    global $col;
+    $x = $x1;
+    $y = $y1;
+
+    if ($x1 < $x2)
+    {
+        $xi = 1;
+        $dx = $x2 - $x1;
+    }
+    else
+    {
+        $xi = -1;
+        $dx = $x1 - $x2;
+    }
+
+    if ($y1 < $y2)
+    {
+        $yi = 1;
+        $dy = $y2 - $y1;
+    }
+    else
+    {
+        $yi = -1;
+        $dy = $y1 - $y2;
+    }
+
+    $col[$x][$y] = "white";
+    $is_dx_greater = $dx > $dy;
+    $ai = ($is_dx_greater ? ($dy - $dx) * 2 : ( $dx - $dy ) * 2);
+    $bi = ($is_dx_greater ? $dx * 2 : $dy * 2);
+    $d = ($is_dx_greater ? $bi - $dx : $bi - $dy);
+
+    while (($x != $x2) || ($y != $y2))
+    {
+
+        if ($d >= 0)
+        {
+            $x += $xi;
+            $y += $yi;
+            $d += $ai;
         }
-
-        .green {
-            background-color: green;
+        else
+        {
+            $d += $bi;
+            if ($is_dx_greater) {
+                $x += $xi;
+            } else {
+                $y += $yi;
+            }
         }
+        $col[$x][$y] = "white";
+    }
+}
 
-        .gray {
-            background-color: gray;
-        }
-
-        .white {
-            background-color: white;
-        }
-    </style>
-</head>
-<body>
-
-<div>
-    <a class="block gray" href="?x=0&z=0"></a>
-    <a class="block gray" href="?x=1&z=0"></a>
-    <a class="block gray" href="?x=2&z=0"></a>
-</div>
-<div>
-    <a class="block gray" href="?x=0&z=1"></a>
-    <a class="block gray" href="?x=1&z=1"></a>
-    <a class="block gray" href="?x=2&z=1"></a>
-</div>
-<div>
-    <a class="block gray" href="?x=0&z=2"></a>
-    <a class="block gray" href="?x=1&z=2"></a>
-    <a class="block gray" href="?x=2&z=2"></a>
-</div>
-
-<br/>
-
-<form method="post">
-    <label>
-        Columns:
-        <input type="text" name="sx">
-    </label>
-    <label>
-        Rows:
-        <input type="text" name="sz">
-    </label>
-    <input type="submit" value="Change">
-</form>
-
-<form method="post">
-    <label>
-        Color:
-        <select name="color">
-            <option value="gray">Gray</option>
-            <option value="red">Red</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-        </select>
-    </label>
-    <input type="submit" value="Change">
-</form>
-
-</body>
-</html>
+?>
